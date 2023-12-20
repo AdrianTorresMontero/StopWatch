d0c9decef2cef6fc3995c48fa6b26288c03f56b8
Adrian Torres <torresadriian@gmail.com>
2023-12-20 15:16:01 -0500

    clock working

---
+++ scripts/stopwatch.js
@@ -0,0 +1,40 @@
+document.querySelector('.js-start')
+  .addEventListener('click',()=>{
+    startTimer();
+  })
+
+function startTimer(){
+  let seconds= 0;
+  let minutes= 0;
+  clock = setInterval(()=>{
+    if (seconds<9 && minutes<10){
+      document.querySelector('.js-time')
+        .innerHTML =`0${minutes}:0${seconds+=1}` 
+    }else if(seconds>=9 && seconds<59 && minutes<10){
+      document.querySelector('.js-time')
+        .innerHTML =`0${minutes}:${seconds+=1}` 
+    }else if(seconds>=59 && minutes<10){
+      document.querySelector('.js-time')
+        .innerHTML =`0${minutes+=1}:00`
+      seconds=0
+      //finish 9 minutes
+    }else if(seconds<9 && minutes>=10){
+      document.querySelector('.js-time')
+        .innerHTML =`${minutes}:0${seconds+=1}` 
+    }else if(seconds>=9 && seconds<59 && minutes>=10){
+      document.querySelector('.js-time')
+        .innerHTML =`${minutes}:${seconds+=1}` 
+    }else if(seconds>=59 &&minutes>=10){
+      document.querySelector('.js-time')
+        .innerHTML =`${minutes+=1}:00`
+      seconds=0
+      //finish 59 minutes
+    }
+    
+  },1)
+  if(document.querySelector('.js-time').innerHTML=== '59:59'){
+    clearInterval(clock);
+  }
+  //clearInterval(clock);
+  console.log(clock);
+}
